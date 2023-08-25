65b4d650b6ec7ea887f2d345d9cb1e6f16fbabd7
Sven Johnson <sjohnson@sjohnson.io>
2023-08-25 04:18:54 +0300

    reverted all the junk changes to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,29 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {AuthModule} from './auth/auth.module';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {typeOrmConfig} from './config/database.config';
-import {ConfigModule, ConfigService} from '@nestjs/config';
-import {ReportsModule} from './reports/reports.module';
-import {DebugModule} from './debug/debug.module';
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database.module';
+import { DiabloItemModule } from './diablo-items/diablo-item.module';
+import { ServiceSlotsModule } from './services/service-slots/service-slots.module';
+import { ServicesModule } from './services/services.module';
+import { TradeModule } from './trade/trade.module';
+import { ReportsModule } from './reports/reports.module';
+import { DebugModule } from './debug/debug.module';
 
 @Module({
     imports: [
+        DatabaseModule,
+        ScheduleModule.forRoot(),
         AuthModule,
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule], // Import the ConfigModule for using the ConfigService
-            inject: [ConfigService], // Inject the ConfigService into the factory function
-            useFactory: async (configService: ConfigService) => {
-                await ConfigModule.envVariablesLoaded;
-                return typeOrmConfig(configService);
-            },
-        }),
-        ConfigModule.forRoot(),
+        DiabloItemModule,
+        TradeModule,
+        ServicesModule,
+        ServiceSlotsModule,
         ReportsModule,
-        DebugModule],
+        DebugModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
