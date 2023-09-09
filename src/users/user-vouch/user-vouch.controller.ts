9068b92df6e5fa0c9494b5fb9bc72c5095d47e70
devoder <devoder@d4search.com>
2023-09-08 23:29:06 -0400

    Refactor some guards

--- src/users/user-vouch/user-vouch.controller.ts
+++ src/users/user-vouch/user-vouch.controller.ts
@@ -4,19 +4,18 @@ import { RequestModel } from 'src/auth/request.model';
 import { CloseUserVouchDto } from './close-user-vouch.dto';
 import { UserVouchService } from './user-vouch.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('user/vouch')
 export class UserVouchController {
     constructor(private readonly userVouchService: UserVouchService) {}
 
     @Post('/')
-    @UseGuards(JwtAuthGuard)
     async closeVouch(@Body() createVouchDto: CloseUserVouchDto, @Request() req: RequestModel) {
         const user = req.user;
         return await this.userVouchService.closeVouch(createVouchDto, user);
     }
 
     @Get('/')
-    @UseGuards(JwtAuthGuard)
     async openVouches(@Request() req: RequestModel) {
         const user = req.user;
         return await this.userVouchService.getOpenVouches(user);
